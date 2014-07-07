40d1940aed38fe7e1081caf823e33cab35d5cc21
David Wolfe <dwolfe66@hotmail.com>
2014-07-07 13:42:58 -0700

    sentiment_1st pass

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -7,8 +8,21 @@ def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
+    # create data dictionairy
     sent_file = open(sys.argv[1])
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+      term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+      scores[term] = int(score)  # Convert the score to an integer.
+
+    print scores.items() # Print every (term, score) pair in the dictionary
     tweet_file = open(sys.argv[2])
+    tweets = {} # dictionary for all tweets
+    for line in tweet_file:
+        tweet = {} # dictionairy for individual tweet
+        tweet = json.loads(line)    
+        print tweet.items()
+        
     hw()
     lines(sent_file)
     lines(tweet_file)

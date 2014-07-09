3887936f390fa8b234ffe0b5800538c21c735984
David Wolfe <dwolfe66@hotmail.com>
2014-07-09 15:31:40 -0700

    added sentiment score token calculator sub method

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -18,24 +18,41 @@ def main():
     # print scores.items() # Print every (term, score) pair in the dictionary
     tweet_file = open(sys.argv[2])
     tweets = {} # dictionary for all tweets
+    tweetindex = 0
     for line in tweet_file:
+        tweetindex = tweetindex +1
+        tweetscore = 0
         tweet = {} # dictionairy for individual tweet
         tweet = json.loads(line)    
         if tweet.get('text','NO') != 'NO': 
             try:
-                print tweet['text']
+                tweetTerm = tweet['text']
+                tweetscore = getTweetScore(tweetTerm, scores)
+                # unneeded
+                print tweetTerm
+                print 'score='+str(tweetscore)
                 print tweet['id_str']
                 print tweet['user']['id']
                 print tweet['user']['name']
                 print '--------------------------'
             except UnicodeEncodeError:
-                print "Unicode error"
+                print "Unicode error" +str(tweetscore)
         else:
             print tweet.items()
-        
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+        tweets[tweetindex] = tweetscore
+
+    print tweets    
+    #hw()
+    # lines(sent_file)
+    # lines(tweet_file)
+
+def getTweetScore(twText , scores):
+    score = 0;
+    tweetTerms = twText.split()
+    for item in tweetTerms:
+        if scores.get(item, 'NOT FOUND') != 'NOT FOUND':
+            score = score + scores[item]
+    return score
 
 if __name__ == '__main__':
     main()

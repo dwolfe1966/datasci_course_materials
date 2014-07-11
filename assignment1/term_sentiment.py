1f58cf609a5b1f04e73a5e9f502dec668f3539e0
David Wolfe <dwolfe66@hotmail.com>
2014-07-11 12:44:49 -0700

    sentiment score doc & term_sentiment

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,39 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
 
 def main():
+    # create data dictionairy
     sent_file = open(sys.argv[1])
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+      term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+      scores[term] = int(score)  # Convert the score to an integer.
+
+    # print scores.items() # Print every (term, score) pair in the dictionary
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    e = 0
+    for line in tweet_file:
+        tweetindex = tweetindex +1
+        tweetscore = 0
+        tweet = {} # dictionairy for individual tweet
+        tweet = json.loads(line)    
+        if tweet.get('text','NO') != 'NO': 
+            try:
+                tweetText = tweet['text']
+                tweetscore = getTweetScore(tweetText, scores)
+                # TODO - CREATE SCORES FOR WORDS SURROUNDING SUCCESSFULLY SCORED TERMS
+            except UnicodeEncodeError:
+                e = e+1
+                   
+
+def getTweetScore(twText , scores):
+    score = 0;
+    tweetTerms = twText.split()
+    for item in tweetTerms:
+        if scores.get(item, 'NOT FOUND') != 'NOT FOUND':
+            score = score + scores[item]
+    return score
 
 if __name__ == '__main__':
     main()
